788297ee35f661080b5edee57a1f7edde3a6b6df
Max YT <161175370+MaxYT5184@users.noreply.github.com>
2025-08-12 11:50:37 -0700

    Delete js directory

--- js/loading.js
+++
@@ -1,6 +0,0 @@
-window.onload = () => {
-  setTimeout(() => {
-    document.getElementById('loading-screen').style.display = 'none';
-    document.getElementById('main-content').style.display = 'block';
-  }, 2000);
-};
--- js/main.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('footer').innerHTML = `
-  <div class="footer-content">
-    <p>© 2025 Max Crafter YT. All rights reserved.</p>
-    <p><a href="pages/tos.html">Terms of Service</a> | <a href="pages/privacy.html">Privacy Policy</a></p>
-  </div>
-`;
